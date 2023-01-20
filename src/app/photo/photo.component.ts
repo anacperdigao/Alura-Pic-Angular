820d4ad68e7d06cac77befdcb3fb6ec60944744c
anacperdigao <anacarolinaperdigao1@gmail.com>
2023-01-20 14:30:35 -0300

    creating photo-component

---
+++ src/app/photo/photo.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'ap-photo',
+  templateUrl: 'photo.component.html'
+})
+
+export class PhotoComponent {
+  description = 'Leão';
+  url = 'https://www.petz.com.br/blog/wp-content/uploads/2022/01/curiosidades-sobre-leao2.jpg'
+}
